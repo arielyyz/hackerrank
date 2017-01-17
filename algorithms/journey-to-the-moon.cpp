c50e5b2ac6e293025f1d13e63869683eebd994cc
Ariel Yosef <ariel.yyz@gmail.com>
2017-01-17 16:49:34 +0200

    Update journey-to-the-moon.cpp

--- algorithms/journey-to-the-moon.cpp
+++ algorithms/journey-to-the-moon.cpp
@@ -3,21 +3,19 @@
 #include <iostream>
 #include <algorithm>
 
-
 using namespace std;
 
-int main()
-{
+int main(){
     int N, I;
     cin >> N >> I;
-    vector<vector<int> > pairs(N);
-    for (int i = 0; i < I; ++i) {
+    vector< vector<int> > pairs(N);
+    for (int i = 0; i < I; ++i){
         int a, b;
         cin >> a >> b;
         pairs[a].push_back(b);
         pairs[b].push_back(a);
     }
-    bool visited[N]= {false};
+    bool visited[N] = {false};
     vector<int> connected;
     queue<int> q;
     long long singles = 0;
@@ -25,8 +23,8 @@ int main()
         if (visited[i])
             continue;
         visited[i] = 1;
-        if(pairs[i].empty()){
-        	singles++;
+        if(pairs[i].empty());
+	    singles++;
             continue;
         }
         connected.push_back(1);
@@ -35,20 +33,20 @@ int main()
             int t = q.front();
             q.pop();
             for(auto n: pairs[t]){
-                if (!visited[n]) {
-					visited[n] = 1;
-					q.push(n);
-					connected.back()++;
+                if (!visited[n]){
+			visited[n] = 1;
+			q.push(n);
+			connected.back()++;
                 }
             }
-     }
+	}
     }
     long long result = 0;
     long long sum = 0;
     for(int s = 0; s < connected.size(); s++){
     	sum += connected[s];
         for(int t = s + 1; t < connected.size(); t++){
-        result += connected[s] * connected[t];
+        	result += connected[s] * connected[t];
         }
     }
     result += singles * sum;
